c558ea2a4557fc40cc7e8809ea74be4f83a20c52
Chritsian LaForest <christianlaforest45@gmail.com>
2024-06-13 19:00:36 -0400

    the code should work plz plz plz plz plz plz plz

---
+++ otu-mcb-project/src/main.cpp
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2020-2021 OTU-Robomaster
+ *
+ * This file is part of otu-mcb.
+ *
+ * otu-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * otu-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with otu-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifdef PLATFORM_HOSTED
+/* hosted environment (simulator) includes --------------------------------- */
+#include <iostream>
+
+#include "tap/communication/tcp-server/tcp_server.hpp"
+#include "tap/motor/motorsim/sim_handler.hpp"
+#endif
+
+#include "modm/architecture/interface/delay.hpp"
+
+/* arch includes ------------------------------------------------------------*/
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/architecture/profiler.hpp"
+
+/* communication includes ---------------------------------------------------*/
+#include "drivers.hpp"
+#include "drivers_singleton.hpp"
+
+/* error handling includes --------------------------------------------------*/
+#include "tap/errors/create_errors.hpp"
+
+/* control includes ---------------------------------------------------------*/
+#include "tap/architecture/clock.hpp"
+
+// Include for global variables
+#include "global.hpp"
+
+// board
+#include "tap/board/board.hpp"
+
+// include for PID 
+#include "tap/algorithms/smooth_pid.hpp"
+
+// include for motor control
+#include "motor_control.hpp"
+
+
+// Place any sort of input/output initialization here. For example, place
+// serial init stuff here.
+static void initializeIo(src::Drivers *drivers);
+
+// Anything that you would like to be called place here. It will be called
+// very frequently. Use PeriodicMilliTimers if you don't want something to be
+// called as frequently.
+static void updateIo(src::Drivers *drivers);
+
+int main()
+{
+#ifdef PLATFORM_HOSTED
+    std::cout << "Simulation starting..." << std::endl;
+#endif
+
+    /*
+     * NOTE: We are using DoNotUse_getDrivers here because in the main
+     *      robot loop we must access the singleton drivers to update
+     *      IO states and run the scheduler.
+     */
+    src::Drivers *drivers = src::DoNotUse_getDrivers();
+
+    Board::initialize();
+    initializeIo(drivers);
+
+
+#ifdef PLATFORM_HOSTED
+    tap::motorsim::SimHandler::resetMotorSims();
+    // Blocking call, waits until Windows Simulator connects.
+    tap::communication::TCPServer::MainServer()->getConnection();
+#endif
+
+    while (1)
+    {
+        // do this as fast as you can
+        PROFILE(drivers->profiler, updateIo, (drivers));
+
+        if (sendMotorTimeout.execute())
+        {
+            PROFILE(drivers->profiler, drivers->commandScheduler.run, ());
+            PROFILE(drivers->profiler, drivers->djiMotorTxHandler.encodeAndSendCanData, ());
+            PROFILE(drivers->profiler, drivers->terminalSerial.update, ());
+            // take the input from controller, map it to the motors
+            drivers->remote.read();
+
+            set_motor_speeds(drivers->remote);
+        }
+
+        drivers->remote.read();
+        drivers->canRxHandler.pollCanData();
+        modm::delay_us(10);
+        
+    }
+    return 0;
+}
+
+static void initializeIo(src::Drivers *drivers)
+{
+    drivers->can.initialize();
+    drivers->errorController.init();
+    drivers->remote.initialize();
+    drivers->refSerial.initialize();
+    drivers->terminalSerial.initialize();
+    drivers->schedulerTerminalHandler.init();
+    drivers->djiMotorTerminalSerialHandler.init();
+
+    fl_motor.initialize();
+    bl_motor.initialize();
+    br_motor.initialize();
+    fr_motor.initialize();
+    pan_motor.initialize();
+    tilt_motor.initialize();
+    agitator_motor.initialize();
+    left_turret_motor.initialize();
+    right_turret_motor.initialize();
+
+}
+
+static void updateIo(src::Drivers *drivers)
+{
+#ifdef PLATFORM_HOSTED
+    tap::motorsim::SimHandler::updateSims();
+#endif
+
+    drivers->canRxHandler.pollCanData();
+    drivers->refSerial.updateSerial();
+    drivers->remote.read();
+    drivers->mpu6500.read();
+    drivers->remote.read();
+}
---
+++ otu-mcb-project/src/motor_control.hpp
@@ -0,0 +1,94 @@
+#include <math.h>
+
+#include <cstdint>
+
+#include "tap/communication/serial/remote.hpp"
+#include "tap/motor/dji_motor.hpp"
+
+#include "global.hpp"
+
+#define MAX_INT16 32676
+#define MIN_INT16 -32676
+
+using namespace tap::communication::serial;
+
+float deg_to_rad(float deg) { return (deg * 3.14159262) / 180; }
+
+int16_t map(int16_t in, int16_t min1, int16_t max1, int16_t min2, int16_t max2)
+{
+    // int m = (max2 - min2) / (max1 - min1);
+    // int b = max1 - m * min1;
+    // return m * in + b;
+    return (max2 - min2) / (max1 - min1) * (in - min1) + max1;
+}
+
+void set_motor_speeds(const Remote &remote)
+{
+    /*
+     * 1  4 +
+     * 2  3 Y
+     * X  +
+     */
+    // This is for the mouse movement of the turret
+    int mouse_y = remote.getMouseX(),
+        mouse_x = remote.getMouseY();
+    
+    float chassis_angle =
+        deg_to_rad(tap::motor::DjiMotor::encoderToDegrees(pan_motor.getEncoderUnwrapped()));
+
+    int16_t turret_pan =
+        map(remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1, 1, MIN_INT16, MAX_INT16) + mouse_x;
+
+    int16_t turret_tilt =
+        map(remote.getChannel(Remote::Channel::RIGHT_VERTICAL), -1, 1, MIN_INT16, MAX_INT16) + mouse_y;
+
+    int16_t turret_x =
+        map(remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1, 1, MIN_INT16, MAX_INT16) +
+        remote.keyPressed(Remote::Key::A) * MIN_INT16 +
+        remote.keyPressed(Remote::Key::D) * MAX_INT16;
+
+    int16_t turret_y =
+        -map(remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1, 1, MIN_INT16, MAX_INT16) +
+        remote.keyPressed(Remote::Key::W) * MIN_INT16 +
+        remote.keyPressed(Remote::Key::S) * MAX_INT16;
+
+    int16_t chassis_x = turret_x * std::cos(chassis_angle) - turret_y * std::sin(chassis_angle);
+    int16_t chassis_y = turret_x * std::sin(chassis_angle) + turret_y * std::cos(chassis_angle);
+
+    // TODO: add rotation factor to make the robot rotate
+    int c1 = (chassis_y + chassis_x);
+    int c2 = (chassis_y - chassis_x);
+    int c3 = -(chassis_y + chassis_x);
+    int c4 = -(chassis_y - chassis_x);
+
+    // lock the rotation of the chassis and turret
+    if (remote.getSwitch(Remote::Switch::RIGHT_SWITCH) == Remote::SwitchState::UP)
+    {
+        c1 += turret_pan;
+        c2 += turret_pan;
+        c3 += turret_pan;
+        c4 += turret_pan;
+    }
+    else
+    {
+        // TODO: subtract the rotation factor to make the  turret counter rotate
+        pan_motor.setDesiredOutput(turret_pan);
+    }
+
+    tilt_motor.setDesiredOutput(turret_tilt);
+    fl_motor.setDesiredOutput(c1);
+    bl_motor.setDesiredOutput(c2);
+    fr_motor.setDesiredOutput(c3);
+    br_motor.setDesiredOutput(c4);
+
+    // This is all for shooting the turret
+    if (remote.getMouseL())
+    {
+        agitator_motor.setDesiredOutput(MAX_INT16);
+    }
+    if (remote.getMouseR())
+    {
+        left_turret_motor.setDesiredOutput(MAX_INT16);
+        right_turret_motor.setDesiredOutput(MAX_INT16);
+    }
+}
