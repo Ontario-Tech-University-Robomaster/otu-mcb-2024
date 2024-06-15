ecfc5ec00fcf9f7f8c7f39f01adeaae5b2dfc71e
Chritsian LaForest <christianlaforest45@gmail.com>
2024-06-14 22:19:08 -0400

    I don't know why the turret isn't working, probably an issue with the gm6020's?

--- otu-mcb-project/src/global.cpp
+++ otu-mcb-project/src/global.cpp
@@ -11,7 +11,7 @@
 // static constexpr tap::motor::MotorId agitator_MOTOR_ID = tap::motor::MOTOR3;
 
 // static constexpr tap::can::CanBus CAN_BUS = tap::can::CanBus::CAN_BUS1;
-// static constexpr tap::can::CanBus CAN2_BUS = tap::can::CanBus::CAN_BUS2;
+// static constexpr tap::can::CanBus CAN_BUS2 = tap::can::CanBus::CAN_BUS2;
 
 // tap::motor::DjiMotor fl_motor(src::DoNotUse_getDrivers(), FRONT_LEFT_MOTOR_ID, CAN_BUS, false, "front left");
 // tap::motor::DjiMotor bl_motor(src::DoNotUse_getDrivers(), BACK_LEFT_MOTOR_ID, CAN_BUS, false, "back left");
@@ -20,9 +20,9 @@
 // tap::motor::DjiMotor pan_motor(src::DoNotUse_getDrivers(), PAN_MOTOR_ID, CAN_BUS, false, "pan");
 // tap::motor::DjiMotor tilt_motor(src::DoNotUse_getDrivers(), TILT_MOTOR_ID, CAN_BUS, false, "tilt");
 
-// tap::motor::DjiMotor right_turret_motor(src::DoNotUse_getDrivers(), right_shooter_MOTOR_ID, CAN2_BUS, false, "right shooter");
-// tap::motor::DjiMotor left_turret_motor(src::DoNotUse_getDrivers(), left_shooter_MOTOR_ID, CAN2_BUS, false, "left shooter");
-// tap::motor::DjiMotor agitator_motor(src::DoNotUse_getDrivers(), agitator_MOTOR_ID, CAN2_BUS, false, "agitator");
+// tap::motor::DjiMotor right_turret_motor(src::DoNotUse_getDrivers(), right_shooter_MOTOR_ID, CAN_BUS2, false, "right shooter");
+// tap::motor::DjiMotor left_turret_motor(src::DoNotUse_getDrivers(), left_shooter_MOTOR_ID, CAN_BUS2, false, "left shooter");
+// tap::motor::DjiMotor agitator_motor(src::DoNotUse_getDrivers(), agitator_MOTOR_ID, CAN_BUS2, false, "agitator");
 
 // // どぞよろしく
 
--- otu-mcb-project/src/motor_control.hpp
+++ otu-mcb-project/src/motor_control.hpp
@@ -7,8 +7,11 @@
 
 #include "global.hpp"
 
+#define SCALE 0.3
+
 #define MAX_INT16 32676
-#define MIN_INT16 -32676
+#define MAX_SPEED ((int)(MAX_INT16 * SCALE))
+#define MIN_SPEED -((int)(MAX_INT16 * SCALE))
 
 using namespace tap::communication::serial;
 
@@ -37,20 +40,16 @@ void set_motor_speeds(const Remote &remote)
         deg_to_rad(tap::motor::DjiMotor::encoderToDegrees(pan_motor.getEncoderUnwrapped()));
 
     int16_t turret_pan =
-        map(remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1, 1, MIN_INT16, MAX_INT16) + mouse_x;
+        map(remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1, 1, MIN_SPEED, MAX_SPEED);
 
     int16_t turret_tilt =
-        map(remote.getChannel(Remote::Channel::RIGHT_VERTICAL), -1, 1, MIN_INT16, MAX_INT16) + mouse_y;
+        map(remote.getChannel(Remote::Channel::RIGHT_VERTICAL), -1, 1, MIN_SPEED, MAX_SPEED);
 
     int16_t turret_x =
-        map(remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1, 1, MIN_INT16, MAX_INT16) +
-        remote.keyPressed(Remote::Key::A) * MIN_INT16 +
-        remote.keyPressed(Remote::Key::D) * MAX_INT16;
+        map(remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1, 1, MIN_SPEED, MAX_SPEED);
 
     int16_t turret_y =
-        -map(remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1, 1, MIN_INT16, MAX_INT16) +
-        remote.keyPressed(Remote::Key::W) * MIN_INT16 +
-        remote.keyPressed(Remote::Key::S) * MAX_INT16;
+        -map(remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1, 1, MIN_SPEED, MAX_SPEED);
 
     int16_t chassis_x = turret_x * std::cos(chassis_angle) - turret_y * std::sin(chassis_angle);
     int16_t chassis_y = turret_x * std::sin(chassis_angle) + turret_y * std::cos(chassis_angle);
@@ -61,19 +60,8 @@ void set_motor_speeds(const Remote &remote)
     int c3 = -(chassis_y + chassis_x);
     int c4 = -(chassis_y - chassis_x);
 
-    // lock the rotation of the chassis and turret
-    // if (remote.getSwitch(Remote::Switch::RIGHT_SWITCH) == Remote::SwitchState::UP)
-    // {
-    //     c1 += turret_pan;
-    //     c2 += turret_pan;
-    //     c3 += turret_pan;
-    //     c4 += turret_pan;
-    // }
-    // else
-    {
-        // TODO: subtract the rotation factor to make the  turret counter rotate
-        pan_motor.setDesiredOutput(turret_pan);
-    }
+    // TODO: subtract the rotation factor to make the  turret counter rotate
+    pan_motor.setDesiredOutput(turret_pan);
 
     tilt_motor.setDesiredOutput(turret_tilt);
     fl_motor.setDesiredOutput(c1);
@@ -81,25 +69,10 @@ void set_motor_speeds(const Remote &remote)
     fr_motor.setDesiredOutput(c3);
     br_motor.setDesiredOutput(c4);
 
-    // This is all for shooting the turret
-    // if (remote.getMouseL() || remote.getSwitch())
-    // {
-        // agitator_motor.setDesiredOutput(MAX_INT16);
-    // }
-    // if (remote.getMouseR())
-    // {
-        // left_turret_motor.setDesiredOutput(MAX_INT16);
-        // right_turret_motor.setDesiredOutput(MAX_INT16);
-    // }
-    left_turret_motor.setDesiredOutput(MAX_INT16);
-    right_turret_motor.setDesiredOutput(-MAX_INT16);
+    left_turret_motor.setDesiredOutput(MAX_SPEED);
+    right_turret_motor.setDesiredOutput(-MAX_SPEED);
     if (remote.getSwitch(Remote::Switch::RIGHT_SWITCH) == Remote::SwitchState::UP)
     {
-        agitator_motor.setDesiredOutput(MAX_INT16);
+        agitator_motor.setDesiredOutput(MAX_SPEED);
     }
-    // if (remote.getMouseR())
-    // {
-    //     left_turret_motor.setDesiredOutput(MAX_INT16);
-    //     right_turret_motor.setDesiredOutput(MAX_INT16);
-    // }
 }
